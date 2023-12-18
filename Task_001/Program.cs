ecd4fd2266aa31b91198d932743ad7e8db6a7d1e
slavasob@gmail.com <slavasob@gmail.com>
2023-12-18 18:56:19 +0300

    Initial commit

---
+++ Task_001/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+
+int rowPosition = ReadInt("Vvedite  nomer stroki elementa v massive: ");
+int colPosition = ReadInt("Vvedite  nomer stolbtsa elementa v massive: ");
+
+
+int[,] GenerateMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] tempmatrix = new int[row, col];
+    Random rand = new Random();
+    for (int i = 0; i < tempmatrix.GetLength(0); i++)
+        for (int j = 0; j < tempmatrix.GetLength(1); j++)
+            tempmatrix[i, j] = rand.Next(leftRange, rightRange + 1);
+
+    return tempmatrix;
+
+}
+
+int[,] matrix = GenerateMatrix(5, 7, -9, 9);
+
+
+
+
+
+int result = GetArrayElement(matrix, rowPosition, colPosition);
+
+if (result != int.MinValue)
+{
+    Console.WriteLine($"Znachenie elementa: {result}");
+}
+else
+{
+    Console.WriteLine("Takogo elementa net v massive");
+}
+
+int GetArrayElement(int[,] matrix, int row, int col)
+{
+    // Проверка наличия строки и столбца в массиве
+    if (row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1))
+    {
+        // Возвращение значения элемента
+        return matrix[row, col];
+    }
+    else
+    {
+        // Возвращение значения int.MinValue, чтобы обозначить отсутствие элемента
+        return int.MinValue;
+    }
+}
---
+++ Task_002/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 2: Задайте двумерный массив. 
+// Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int[,] GenerateMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] tempmatrix = new int[row, col];
+    Random rand = new Random();
+    for (int i = 0; i < tempmatrix.GetLength(0); i++)
+        for (int j = 0; j < tempmatrix.GetLength(1); j++)
+            tempmatrix[i, j] = rand.Next(leftRange, rightRange + 1);
+
+    return tempmatrix;
+
+}
+
+int[,] matrix = GenerateMatrix(5, 5, -9, 9);
+
+void SwapFirstAndLastRows(int[,] matrix)
+{
+    int rowCount = matrix.GetLength(0);
+    int colCount = matrix.GetLength(1);
+
+    // Проверка, что в массиве есть хотя бы две строки
+    if (rowCount >= 2)
+    {
+        // Меняем местами первую и последнюю строки
+        for (int j = 0; j < colCount; j++)
+        {
+            int temp = matrix[0, j];
+            matrix[0, j] = matrix[rowCount - 1, j];
+            matrix[rowCount - 1, j] = temp;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Недостаточно строк для обмена");
+    }
+}
+
+void PrintMatrix(int[,] matrixForPrint)
+{
+    for (int i = 0; i < matrixForPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixForPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixForPrint[i, j] + "\t");
+
+        }
+        System.Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Ishodniy massiv:");
+PrintMatrix(matrix);
+
+SwapFirstAndLastRows(matrix);
+
+Console.WriteLine("Massiv posle obmena:");
+PrintMatrix(matrix);
